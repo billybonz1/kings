fd235128442aeb31e13f7d6d23889fbe65fb2e91
Ruslan <lumini.ferous.19@gmail.com>
2020-12-15 19:38:59 +0200

    111

---
+++ treefamily/famdata.js
@@ -0,0 +1,42 @@
+﻿// var famdata = [
+//     { id: 29, title: "Иванов Дмитрий Сергеевич ", label: "Thomas Williams", description: "Р.Хакассия, П.Майна, 17.05.2015-17.05.2215", phone: "262-215-7998", email: "jameholt@name.com", image: "photos/e.png", itemTitleColor: "#c8e4fb" },
+// 	 { id: 28, title: "Иванов Дмитрий Сергеевич ", label: "Thomas Williams", description: "Р.Хакассия, П.Майна, 17.05.2015-17.05.2215", phone: "262-215-7998", email: "jameholt@name.com", image: "photos/e.png", itemTitleColor: "#c8e4fb" },
+// 	 { id: 30,parents: [29, 28], title: "", label: "Thomas Williams", description: "", phone: "262-215-7998", email: "jameholt@name.com", image: "", itemTitleColor: "#c8e4fb" },
+	
+// 	{ id: 3, title: "Иванов Дмитрий Сергеевич ", label: "Thomas Williams", description: "Р.Хакассия, П.Майна, 17.05.2015-17.05.2215", phone: "262-215-7998", email: "jameholt@name.com", image: "photos/e.png", itemTitleColor: "#c8e4fb" },
+
+//     { id: 4, title: "Белозёров Боян Филиппович", label: "Thomas Williams", description: "4, VP, Server & Tools Marketing and Solutions", phone: "904-547-5342", email: "thomwill@name.com", image: "photos/r.png", itemTitleColor: "#c8e4fb" },
+   
+//    { id: 5, title: "David Kirby", label: "David Kirby", description: "5, AGM, Infrastructure Server and IT Pro Audience Marketing", phone: "614-395-7238", email: "davikirb@name.com", image: "photos/t.png", itemTitleColor: "#c8e4fb" },
+//    { id: 6,   title: "Lynette Maloney", label: "Lynette Maloney", description: "6, GM, Name.com Experience Platforms and Solutions", phone: "540-822-3862", email: "lynemalo@name.com", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+	
+//    { id: 10, parents: [3, 4], title: "Heather Simmons", label: "Heather Simmons", description: "10, GM, Name Learning", phone: "620-368-3620", email: "heatsimm@name.com", image: "photos/p.png", itemTitleColor: "#c8e4fb" },
+//     { id: 11, parents: [3, 4], title: "Steven Lacombe", label: "Steven Lacombe", description: "11, GM, Platform Strategy", phone: "805-800-7397", email: "stevlaco@name.com", image: "photos/a.png", itemTitleColor: "#ffc0cb" },
+   
+   
+//    { id: 13, parents: [5, 6], title: "Шалдыбин Григорий Борисович", label: "Jean Kendall", description: "13, GM, DML Server Marketing", phone: "949-453-0415", email: "jeankend@name.com", image: "photos/d.png", itemTitleColor: "#c8e4fb" },
+//    { id: 14, parents: [5, 6], title: "Борисова Полина Виталиевна ", label: "Brad Whitt", description: "14, GM, Application Platform and Development Marketing", phone: "502-528-6379", email: "bradwhit@name.com", image: "photos/f.png", itemTitleColor: "#c8e4fb" },
+
+// 	{ id: 25, parents: [11, 14], title: "Наталья Васильевна Тронина", label: "Ralph Mercer", description: "22, GM, DML Server Strategy", phone: "814-327-5895", email: "ralpmerc@name.com", image: "photos/c.png", itemTitleColor: "#c8e4fb" },
+// 	{ id: 22, parents: [11, 14], title: "Наталья Васильевна Тронина", label: "Ralph Mercer", description: "22, GM, DML Server Strategy", phone: "814-327-5895", email: "ralpmerc@name.com", image: "photos/c.png", itemTitleColor: "#c8e4fb" },
+//     { id: 23, parents: [11, 14], title: "Дидиченко Варлаам Павлович", label: "Howard Williams", description: "23, GM, User Experience", phone: "703-740-8612", email: "howawill@name.com", image: "photos/v.png", itemTitleColor: "#c8e4fb" },
+//     { id: 24, parents: [11, 14], title: "Nathalie Escobedo", label: "Nathalie Escobedo", description: "24, GM, DML Server Business Intelligence", phone: "504-555-8165", email: "nathesco@name.com", image: "photos/b.png", itemTitleColor: "#c8e4fb" },
+
+// 	    { id: 26, parents: [13], title: "Howard Williams", label: "Howard Williams", description: "23, GM, User Experience", phone: "703-740-8612", email: "howawill@name.com", image: "photos/v.png", itemTitleColor: "#c8e4fb" },
+// ];
+
+
+
+var famdata = [
+  { id: 1, title: "Рюрик", label: "Рюрик", description: "", image: "", itemTitleColor: "#c8e4fb" },
+  { id: 2, title: "Рюрик", label: "Рюрик", description: "", image: "", itemTitleColor: "#c8e4fb" }
+
+
+  { id: 1, title: "Борис", label: "Борис", description: "Сын Равноапостольного Князя Киевского Владимира и его Супурги Порфирородной Царицы Византийской Анны", image: "photos/e.png", itemTitleColor: "#c8e4fb" },
+
+  { id: 2, title: "Глеб", label: "Глеб", description: "Сын Равноапостольного Князя Киевского Владимира и его Супурги Порфирородной Царицы Византийской Анны", image: "photos/e.png", itemTitleColor: "#c8e4fb" },
+
+
+
+];
---
+++ treefamily/index.html
@@ -0,0 +1,68 @@
+﻿<!DOCTYPE html> 
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Resize Example</title>
+    <link rel="stylesheet" href="ui-lightness/jquery-ui-1.10.2.custom.css" />
+    <script type="text/javascript" src="jquery-1.9.1.js"></script>
+    <script type="text/javascript" src="jquery-ui-1.10.2.custom.min.js"></script>
+
+    <script  type="text/javascript" src="primitives.min.js?2020"></script>
+    <link href="primitives.latest.css?2020" media="screen" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="famdata.js"></script>
+	<script src="jquery.ui.touch-punch.min"></script>
+	
+	<style>#draggable {cursor: help;}       </style>
+<script>$(function() {$( "#draggable" ).draggable();        });</script>
+
+    <script type="text/javascript">
+        jQuery(document).ready(function () {
+            var options = new primitives.orgdiagram.Config();
+
+            options.items = famdata;
+			
+			
+
+            options.cursorItem = null;
+            options.hasSelectorCheckbox = primitives.common.Enabled.False;
+            options.hasButtons = primitives.common.Enabled.False;
+            options.pageFitMode = primitives.common.PageFitMode.PrintPreview;
+            options.elbowType = primitives.common.ElbowType.Round;
+            options.normalLevelShift = 20;
+            options.dotLevelShift = 20;
+            options.lineLevelShift = 24;
+            options.normalItemsInterval = 10;
+            options.dotItemsInterval = 1;
+            options.lineItemsInterval = 1;
+            options.linesWidth = 1;
+            options.linesColor = "#7C8993";
+        
+			
+			
+            jQuery("#diagram").famDiagram(options);
+
+			
+			
+            var placeholder = jQuery(".placeholder");
+            jQuery("#diagram").css({
+                width: placeholder.width() + 10,
+                height: placeholder.height() + 10,
+            })
+            jQuery("#diagram").famDiagram("update");
+			
+			
+			
+
+        });
+		
+
+    </script>
+</head>
+<body>
+<div class="container">
+	<div id="draggable" class="ui-widget-content ui-draggable">
+    <div id="diagram" style="width:1000px; height:1000px"></div>
+	</div>
+	</div>
+</body>
+</html>
